8214fcac9b78e33f2713a58d08296e25f0441d0b
vasu580 <natinavasu9@gmal.com.com>
2026-03-19 14:42:12 +0530

    Employee Management system project

---
+++ Employee_management_system/frontend/src/components/Header.jsx
@@ -0,0 +1,26 @@
+import {useNavigate} from 'react-router-dom';
+function Header()
+{
+    const navigate = useNavigate();
+
+    const handleLogout = (e)=>{
+        e.preventDefault();
+        localStorage.removeItem("logged");
+        navigate("/login");
+    }
+
+    return(
+       <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+        <div className="container">
+            <a href="" className="navbar-brand fw-bold"> BridgeSoft </a>
+            {
+                localStorage.getItem("logged") && 
+                <button type="button" className="btn btn-primary" onClick={handleLogout}>
+                     Logout 
+                </button>
+            }
+        </div>
+       </nav>             
+    )
+}
+export default Header;
